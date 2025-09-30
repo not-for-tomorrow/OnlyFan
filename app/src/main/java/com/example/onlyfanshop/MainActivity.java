fef73d33dfd4d76ad951cc0c2bc04fbc8aff02d1
Nguyễn Hoàng Thiên <149929660+Thienx2004@users.noreply.github.com>
2025-09-30 16:30:50 +0700

    create Forgot password fearture

--- app/src/main/java/com/example/onlyfanshop/MainActivity.java
+++ app/src/main/java/com/example/onlyfanshop/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.onlyfanshop;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.onlyfanshop.api.ApiClient;
 import com.example.onlyfanshop.api.UserApi;
+import com.example.onlyfanshop.model.UserDTO;
 
 import java.util.List;
 
@@ -18,11 +20,15 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private UserApi userApi;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        UserDTO user = (UserDTO) getIntent().getSerializableExtra("user");
+        textView = findViewById(R.id.textView);
+        textView.setText("Welcome " + user.getUsername());
+        userApi = ApiClient.getClient().create(UserApi.class);
     }
 }
 
--- app/src/main/java/com/example/onlyfanshop/model/LoginRequest.java
+++ app/src/main/java/com/example/onlyfanshop/model/LoginRequest.java
@@ -2,17 +2,17 @@ package com.example.onlyfanshop.model;
 
 
 public class LoginRequest {
-    private String username;
+    private String email;
     private String password;
 
-    public LoginRequest(String username, String password) {
-        this.username = username;
+    public LoginRequest(String email, String password) {
+        this.email = email;
         this.password = password;
     }
 
     // getters & setters
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
 }
